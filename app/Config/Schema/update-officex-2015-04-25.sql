7492a2243621c60a90643f77acdd947fae8bb390
jdecode <jdecode@gmail.com>
2015-04-25 08:41:09 +0530

    Added latest DB dump

--- app/Config/Schema/update-officex-2015-04-25.sql
+++ app/Config/Schema/update-officex-2015-04-25.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 25, 2015 at 04:35 AM
+-- Generation Time: Apr 25, 2015 at 08:39 AM
 -- Server version: 5.5.43-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.9
 
@@ -53,6 +53,7 @@ INSERT INTO `departments` (`id`, `name`, `status`, `created`, `modified`) VALUES
 
 CREATE TABLE IF NOT EXISTS `documents` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
+  `message_id` int(8) NOT NULL DEFAULT '0',
   `name` text CHARACTER SET utf8 NOT NULL,
   `filename` varchar(64) CHARACTER SET utf8 NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -60,7 +61,21 @@ CREATE TABLE IF NOT EXISTS `documents` (
   `modified` int(10) NOT NULL,
   `created` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`id`, `message_id`, `name`, `filename`, `title`, `status`, `modified`, `created`) VALUES
+(1, 40, '11136235_795111837246572_2492016457494034586_o.jpg', 'c1417788bfffa7d0ef983388eae46ca4554cf162.jpg', '11136235_795111837246572_2492016457494034586_o.jpg', 1, 1429929635, 1429929635),
+(2, 40, '11074307_795106563913766_1974788765776105981_o.jpg', 'de2429892c5bda975a72d597e849c23cfa63deb6.jpg', '11074307_795106563913766_1974788765776105981_o.jpg', 1, 1429929635, 1429929635),
+(3, 40, '11072337_794230964001326_3406627774466798488_o.jpg', '7b1a20f4bc8d0c69ac5240b3fdca6991510f8acb.jpg', '11072337_794230964001326_3406627774466798488_o.jpg', 1, 1429929635, 1429929635),
+(4, 40, '11015656_852202671493313_1996546185_n.jpg', '5e5ef4c8b31c975a3d19d4d8f58dbb99ec0110be.jpg', '11015656_852202671493313_1996546185_n.jpg', 1, 1429929635, 1429929635),
+(5, 41, '11149608_896856683691584_4352970284522865110_o.jpg', 'ec9fae5ee1abaf9770017621c9634d3f7ab9ab39.jpg', '11149608_896856683691584_4352970284522865110_o.jpg', 1, 1429929745, 1429929745),
+(6, 41, '11146346_896853550358564_7753765832477494150_o.jpg', '082bc5c430e2e054e72013d3d2203aead57e6c7c.jpg', '11146346_896853550358564_7753765832477494150_o.jpg', 1, 1429929745, 1429929745),
+(7, 41, '11041883_896853517025234_372666254274391957_o.jpg', '84224cb76e7022e77121d9aabab606c46600716d.jpg', '11041883_896853517025234_372666254274391957_o.jpg', 1, 1429929745, 1429929745),
+(8, 41, '11155126_896849173692335_7722998124013803128_o.jpg', 'f9ec6de58ada1cb0df54269b04bc5a240cb7af05.jpg', '11155126_896849173692335_7722998124013803128_o.jpg', 1, 1429929745, 1429929745);
 
 -- --------------------------------------------------------
 
@@ -90,11 +105,10 @@ INSERT INTO `folders` (`id`, `name`, `user_id`, `type`, `status`) VALUES
 (21, 'HSE Update', 1, 3, 1),
 (22, 'Project Picture and Progress', 1, 3, 1),
 (23, 'Others', 1, 3, 1),
-(24, 'Received', 1, 2, 1),
-(25, 'Sent', 1, 2, 1),
+(24, 'Received', 1, 2, 0),
+(25, 'Sent', 1, 2, 0),
 (26, 'Uploaded by Scan', 1, 2, 1),
-(27, 'Shared', 1, 2, 1),
-(28, 'Self(Personal)', 1, 2, 1);
+(27, 'Shared', 1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -136,7 +150,15 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `modified` int(10) NOT NULL,
   `created` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `user_id`, `user2id`, `message`, `folder_id`, `status`, `document_id`, `modified`, `created`) VALUES
+(41, 1, 0, 'Draftsss', 0, 0, 0, 1429929745, 1429929745),
+(40, 1, 3, 'Document, Check!', 26, 0, 0, 1429929635, 1429929635);
 
 -- --------------------------------------------------------
 
